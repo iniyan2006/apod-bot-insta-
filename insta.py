faf65c0f25234e6e07ddee1ac43e31585ab7c976
Iniyan <78460090+iniyan2006@users.noreply.github.com>
2022-09-09 06:21:02 +0530

    ...

--- insta.py
+++ insta.py
@@ -6,7 +6,7 @@ import time
 cl = instagrapi.Client()
 cl.login('the_void.sea',os.environ.get('insta_pass'))
 dlr = apod.APOD()
-@schedule.repeat(schedule.every().day.at("06:20"))
+@schedule.repeat(schedule.every().day.at("06:23"))
 def update():
     imgInfo = dlr.img('./')
 
