b02dc1d1f247bbf8701f0c0f20f1d276b7657c54
Iniyan <78460090+iniyan2006@users.noreply.github.com>
2022-09-12 18:06:19 +0530

    corrections ....

--- apod.py
+++ apod.py
@@ -9,12 +9,13 @@ class APOD:
     api_key = os.environ.get('apod_api_key')
     def img(self, file_path):
         data = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={self.api_key}").json()
-        url = data['hdurl']
+        try:
+            url = data['hdurl']
+        except KeyError:
+            url = data['url']
         filename = wget.download(url)
-
-        
         return file_path+filename, data
-    
+
     
     
     def imgByDate(self, date,file_path):
--- insta.py
+++ insta.py
@@ -6,7 +6,7 @@ import time
 cl = instagrapi.Client()
 cl.login('the_void.sea',os.environ.get('insta_pass'))
 dlr = apod.APOD()
-@schedule.repeat(schedule.every().day.at("05:00"))
+@schedule.repeat(schedule.every().day.at("18:07"))
 def update():
     imgInfo = dlr.img('./')
 
